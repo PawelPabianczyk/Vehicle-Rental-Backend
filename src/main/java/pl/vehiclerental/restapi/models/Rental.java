fa83c3118f185c2357e2bda6c6bc7745ea129fa9
Pawel Pabianczyk <pawel.pabianczyk1999@gmail.com>
2021-01-03 11:34:37 +0100

    Add Rental entity

---
+++ src/main/java/pl/vehiclerental/restapi/models/Rental.java
@@ -0,0 +1,61 @@
+package pl.vehiclerental.restapi.models;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(	name = "rentals")
+public class Rental {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+
+    private double cost;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade=CascadeType.PERSIST)
+    @JoinColumn(name="vehicle_id")
+    private Vehicle vehicle;
+
+    public Rental(LocalDate startDate, LocalDate endDate, double cost) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.cost = cost;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+}
