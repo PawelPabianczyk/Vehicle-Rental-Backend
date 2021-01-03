2f2f85868953075f2afa5a8c9d4193013457c763
Pawel Pabianczyk <pawel.pabianczyk1999@gmail.com>
2021-01-03 12:06:37 +0100

    Add Order entity

---
+++ src/main/java/pl/vehiclerental/restapi/models/Order.java
@@ -0,0 +1,76 @@
+package pl.vehiclerental.restapi.models;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(	name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String comments;
+
+    private LocalDate date;
+
+    private double cost;
+
+    private String paymentStatus;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(	name = "order_rentals",
+            joinColumns = @JoinColumn(name = "order_id"),
+            inverseJoinColumns = @JoinColumn(name = "rental_id"))
+    private Set<Rental> rentals = new HashSet<>();
+
+    public Order(String comments, LocalDate date, double cost, String paymentStatus) {
+        this.comments = comments;
+        this.date = date;
+        this.cost = cost;
+        this.paymentStatus = paymentStatus;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public String getPaymentStatus() {
+        return paymentStatus;
+    }
+
+    public void setPaymentStatus(String paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+
+    public Set<Rental> getRentals() {
+        return rentals;
+    }
+
+    public void setRentals(Set<Rental> rentals) {
+        this.rentals = rentals;
+    }
+}
