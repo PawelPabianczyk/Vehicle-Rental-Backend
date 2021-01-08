c3426ea8e40991f6afb5fcf560715b7b5663bf66
Pawel Pabianczyk <pawel.pabianczyk1999@gmail.com>
2021-01-08 19:18:26 +0100

    Add getting user job history

--- src/main/java/pl/vehiclerental/restapi/controllers/JobHistoryController.java
+++ src/main/java/pl/vehiclerental/restapi/controllers/JobHistoryController.java
@@ -6,14 +6,16 @@ import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import pl.vehiclerental.restapi.dtos.EmployeeDto;
+import pl.vehiclerental.restapi.dtos.JobHistoryRecordDto;
+import pl.vehiclerental.restapi.models.Employee;
 import pl.vehiclerental.restapi.models.JobHistoryRecord;
+import pl.vehiclerental.restapi.repository.EmployeeRepository;
 import pl.vehiclerental.restapi.repository.JobHistoryRecordRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -23,11 +25,34 @@ public class JobHistoryController {
     @Autowired
     JobHistoryRecordRepository jobHistoryRecordRepository;
 
+    @Autowired
+    EmployeeRepository employeeRepository;
+
 
     @GetMapping("/all")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> getAllJobHistory() throws JSONException {
         List<JobHistoryRecord> records = jobHistoryRecordRepository.findAll();
+        JSONArray jRecords = new JSONArray();
+        JSONObject jRecord;
+        for (JobHistoryRecord r :
+                records) {
+            jRecord = new JSONObject();
+            jRecord.put("id", r.getId());
+            jRecord.put("startDate", r.getStartDate());
+            jRecord.put("endDate", r.getEndDate());
+            jRecord.put("employeeId", r.getEmployee().getId());
+            jRecords.put(jRecord);
+        }
+
+        return ResponseEntity.ok(jRecords.toString());
+    }
+
+    @GetMapping("/details")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<?> getEmployeeJobHistory(@RequestBody EmployeeDto employeeDto) throws JSONException {
+        Employee employee = employeeRepository.findById(employeeDto.getId()).get();
+        List<JobHistoryRecord> records = jobHistoryRecordRepository.findByEmployee(employee);
         JSONArray jRecords = new JSONArray();
         JSONObject jRecord;
         for (JobHistoryRecord r :
--- src/main/java/pl/vehiclerental/restapi/repository/JobHistoryRecordRepository.java
+++ src/main/java/pl/vehiclerental/restapi/repository/JobHistoryRecordRepository.java
@@ -2,8 +2,15 @@ package pl.vehiclerental.restapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import pl.vehiclerental.restapi.models.Employee;
 import pl.vehiclerental.restapi.models.JobHistoryRecord;
+import pl.vehiclerental.restapi.models.User;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface JobHistoryRecordRepository extends JpaRepository<JobHistoryRecord, Long> {
+    List<JobHistoryRecord> findByEmployee(Employee employee);
+
 }
