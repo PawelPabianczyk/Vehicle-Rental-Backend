6e489cf767c4cd70eaeb08e79ef723ca1779f33d
Pawel Pabianczyk <pawel.pabianczyk1999@gmail.com>
2021-01-22 15:58:31 +0100

    Update tests to TestController

--- src/test/java/pl/vehiclerental/restapi/controllers/TestControllerTest.java
+++ src/test/java/pl/vehiclerental/restapi/controllers/TestControllerTest.java
@@ -1,46 +1,66 @@
 package pl.vehiclerental.restapi.controllers;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TestControllerTest {
 
-    private TestController testController;
+    @Autowired
+    TestController testController;
 
-    @BeforeEach
-    void setUp() {
-        testController = new TestController();
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void contextLoads() throws Exception {
+        assertThat(testController).isNotNull();
     }
 
     @Test
-    void allAccess() {
-        String response = testController.allAccess();
-        assertEquals("Public Content.",response);
+    public void allAccess() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/api/test/all",
+                String.class)).contains("Public Content.");
     }
 
     @Test
+    @WithMockUser(username="user",roles = {"USER"})
     void userAccess() {
         String response = testController.userAccess();
-        assertEquals("User Content.",response);
+        Assert.assertEquals("User Content.", response);
     }
 
     @Test
+    @WithMockUser(username="regular",roles = {"REGULAR"})
     void employeeAccess() {
         String response = testController.employeeAccess();
-        assertEquals("Employee Board.",response);
+        Assert.assertEquals("Employee Board.", response);
     }
 
     @Test
+    @WithMockUser(username="manager",roles = {"MANAGER"})
     void managerAccess() {
         String response = testController.managerAccess();
-        assertEquals("Manager Board.",response);
+        Assert.assertEquals("Manager Board.", response);
     }
 
     @Test
+    @WithMockUser(username="admin",roles = {"ADMIN"})
     void adminAccess() {
         String response = testController.adminAccess();
-        assertEquals("Admin Board.",response);
+        Assert.assertEquals("Admin Board.", response);
     }
 }
